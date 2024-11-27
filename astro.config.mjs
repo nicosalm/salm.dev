b7019088d99da59a165fd58727f55c46e8c079b2
Nico Salm <nico@salm.dev>
2024-11-27 16:25:09 -0600

    [+] Tailwind; feat: Add Header.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: 'min-dark',
+      wrap: true
+    }
+  }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'iosevka': ['var(--font-iosevka)'],
+        'ibm-flexi': ['var(--font-ibm-flexi)'],
+        'ibm-vga': ['var(--font-ibm-vga)'],
+        'bigblue': ['var(--font-bigblue)'],
+      },
+    },
+  },
+  plugins: [],
+}
