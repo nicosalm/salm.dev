8b4de90746b299055f456377b6a91593a150e929
Nico Salm <nico@salm.dev>
2024-11-27 16:25:43 -0600

    feat: Add blog, plus sample post

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    updatedDate: z.date().optional(),
+    author: z.string().default('Your Name'),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }).optional(),
+    tags: z.array(z.string()).default([])
+  })
+});
+
+export const collections = {
+  blog: blog
+};
