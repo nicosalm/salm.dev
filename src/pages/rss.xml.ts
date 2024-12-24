c1c5edbff12d4759380646ad85fe9ce7f99547e5
Nico Salm <nico@salm.dev>
2024-12-24 12:20:48 -0600

    headad

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,26 +1,20 @@
-import rss from '@astrojs/rss';
-import type { APIContext } from 'astro';
 import { getCollection } from 'astro:content';
+import type { APIContext, APIRoute } from 'astro';
 
-export async function GET(context: APIContext) {
-  try {
-    const posts = await getCollection('blog');
-    const items = posts.map((post) => ({
-      link: `/blog/${post.slug}/`,
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-    }));
+export const GET: APIRoute = async (context: APIContext) => {
+    const blog = await getCollection('blog');
+    const rss = (await import('@astrojs/rss')).default;
 
     return rss({
-      title: 'Nico\'s Blog',
-      description: 'Hot takes and cool things.',
-      site: context.site,
-      items: items,
-      customData: `<language>en-us</language>`,
+        title: "Nico's Blog",
+        description: 'Hot takes and cool things',
+        site: context.site,
+        items: blog.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `/blog/${post.slug}/`,
+            categories: post.data.tags
+        }))
     });
-  } catch (error) {
-    console.error('Error generating RSS feed:', error);
-    return new Response('Error generating RSS feed', { status: 500 });
-  }
-}
+};
