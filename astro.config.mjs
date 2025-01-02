0f2ca2b85c941b06f5608ec3499ba516d52fc45b
Nico Salm <nico@salm.dev>
2025-01-01 19:16:50 -0600

    feat: update index.astro with new homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://salm.dev',
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), react()],
   markdown: {
     shikiConfig: {
       theme: 'min-dark',
