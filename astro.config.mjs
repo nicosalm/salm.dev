d94729fe11c506f8bbbac06d1c0614a8da550031
Nico Salm <nico@salm.dev>
2025-01-19 21:59:12 -0600

    feat: The Great Cleansing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,33 @@
-// @ts-check
+//@ts-check
 import { defineConfig } from 'astro/config';
+import expressiveCode from 'astro-expressive-code';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 
-import expressiveCode from 'astro-expressive-code';
-
 export default defineConfig({
   site: 'https://salm.dev',
-  integrations: [   tailwind(),
-                    expressiveCode({
-                        themes: ['andromeeda'],
-                    }),
-                    mdx(),
-                    react()]
+  integrations: [
+    tailwind(),
+    expressiveCode({
+      themes: ['min-dark'],
+      frames: false,
+      styleOverrides: {
+        codeFontSize: '0.9rem',
+        codeFontFamily: 'Iosevka, monospace',
+        borderRadius: '0',
+        borderWidth: '1px',
+        borderColor: '#262626',                 // matches neutral-800
+        codePaddingInline: '1rem',
+        codePaddingBlock: '1rem',
+        codeBackground: '#171717',              // neutral-900
+        codeForeground: '#d1d5db',              // gray-300
+        scrollbarThumbColor: '#262626',         // neutral-800
+        scrollbarThumbHoverColor: '#404040',    // neutral-700
+        focusBorder: 'transparent',
+      }
+    }),
+    mdx(),
+    react()
+  ]
 });
