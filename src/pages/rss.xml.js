eab3cf89d68bf1c2154566e5c95f7a2acdddfd7d
Nico Salm <nico@salm.dev>
2025-02-03 16:24:49 -0600

    Add full content and icon to RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,21 +1,42 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
+
+async function postContent(post) {
+  const html = parser.render(post.body);
+  return sanitizeHtml(html, {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+  });
+}
 
 export async function GET(context) {
   const posts = await getCollection('blog');
-  const sortedPosts = posts.sort((a, b) =>
-    new Date(b.data.date).getTime() - new Date(a.data.date).getTime()
-  );
+  const feed = [];
+
+  for (const post of posts) {
+    feed.push({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+      content: await postContent(post),
+    });
+  }
 
   return rss({
     title: 'Nico Salm',
     description: 'Hot takes and cool things',
     site: context.site,
-    items: sortedPosts.map((post) => ({
-      title: post.data.title,
-      pubDate: new Date(post.data.date),
-      description: post.data.description,
-      link: `/blog/${post.slug}/`,
-    })),
+    items: feed,
+    customData: `
+      <image>
+        <url>${context.site}favicon.svg</url>
+        <title>Nico Salm</title>
+        <link>${context.site}</link>
+      </image>
+    `
   });
 }
