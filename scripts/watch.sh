d6a7a2bd8754138e0c540bc0005787f8a8bd6b3d
Nico Salm <nico@salm.dev>
2025-11-28 22:00:34 -0600

    update links page

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./scripts/build.sh
-find src -type f | entr -r sh -c "./scripts/build.sh && (cd dist && python3 -m http.server 8000)"
+find src -type f | entr -r sh -c "./scripts/build.sh && (cd dist && python3 -m http.server 4321)"
