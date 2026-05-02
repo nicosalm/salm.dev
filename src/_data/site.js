87b3d95ad5cdb1dc2a5c821d800c7896befa3a38
Nico Salm <nico@salm.dev>
2026-05-02 17:56:31 -0500

    rss + meta

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,15 @@
+import { execSync } from "child_process";
+
+const [gitHash, gitDate] = execSync('git log -1 --format="%h %cI"').toString().trim().split(" ");
+
 export default {
   title: "salm.dev",
-  description: "Website of Nico Salm. I study Computer and Data Sciences at UW-Madison. My technical interests include machine learning, multi-agent systems, and performance-oriented backend systems.",
+  description: "Website of Nico Salm. My technical interests are in high-performance computing, algorithms, and full-stack development.",
   url: "https://salm.dev",
   author: { name: "Nico Salm", email: "nico@salm.dev" },
-  currentYear: new Date().getFullYear()
+  git: {
+    hash: gitHash,
+    date: gitDate,
+    url: `https://github.com/nicosalm/salm.dev/commit/${gitHash}`,
+  },
 };
