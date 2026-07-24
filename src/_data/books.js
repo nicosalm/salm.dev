701ac99d00fb91d5222e526e62cb4a23c27d5ffd
Nico Salm <nico@salm.dev>
2026-07-24 11:20:00 -0500

    move the books and web-pages lists into data files

---
+++ src/_data/books.js
@@ -0,0 +1,21 @@
+export default {
+  reading: [
+    { title: "Permutation City", author: "Greg Egan" },
+    { title: "How to Prove It: A Structured Approach", author: "Daniel Velleman" },
+    { title: "Introduction to Algorithms", author: "Cormen et al." },
+  ],
+  read: [
+    { title: "Computer Systems: A Programmer's Perspective", author: "Randal Bryant" },
+    { title: "Designing Data-Intensive Applications", author: "Martin Kleppmann" },
+    { title: "Operating Systems: Three Easy Pieces", author: "Remzi & Andrea Arpaci-Dusseau" },
+    { title: "Computer Networks: A Systems Approach", author: "Peterson & Davie" },
+    { title: "The Soul of a New Machine", author: "Tracy Kidder" },
+    { title: "Weaving the Web", author: "Tim Berners-Lee" },
+  ],
+  toRead: [
+    { title: "Concrete Mathematics", author: "Graham, Knuth & Patashnik" },
+    { title: "The Art of Computer Programming, Vol. 1", author: "Don Knuth" },
+    { title: "Database Internals", author: "Alex Petrov" },
+    { title: "Systems Performance", author: "Brendan Gregg" },
+  ],
+};
